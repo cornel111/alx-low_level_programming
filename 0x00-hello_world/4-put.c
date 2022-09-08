a7116c8af8584095fc89cfe8a5741757d0688398
Ogar Cornelius <corneliusogar.cl@gmail.com>
2022-09-08 07:04:01 -0700

    4-put

---
+++ 0x00-hello_world/4-put.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/*
+ * main - entry point 
+ *
+ * Return: 0 after printing the function
+ * /
+ int main(void)
+ {put ("\"Programming is like budding a multilingual puzzle");
+	return  (0);
+	}
