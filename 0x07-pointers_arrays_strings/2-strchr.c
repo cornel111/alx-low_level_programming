a8b0e1fb41df0ba4002244aab0992582c763bc2d
Ogar Cornelius <corneliusogar.cl@gmail.com>
2022-09-26 10:28:36 -0700

    Task w

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,36 +1,22 @@
 #include "main.h"
-#include <stdio.h>
-
+/**
+*_strchr - Returns a pointer to the first occurrence
+*of the character c in the string s, or NULL if the
+*character is not found
+*
+*@s:string targeted
+*@c:character targeted
+*
+*Return: returns pointer to first occcurence of c
+*/
 char *_strchr(char *s, char c)
 {
-    char *ptr;
-    int i;
+	int i;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        if (s[i] == c)
-        {
-            return (s + i); 
-        }
-    }
-    return ('\0');
-    
+	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
+		;
+	if (s[i] == c)
+		return (s + i);
+	else
+		return (NULL);
 }
-
-int main ()
-{
-
-    char *s = "hello";
-    char *f;
-
-    f = _strchr(s, 'l');
-
-    if (f != NULL)
-    {
-        printf("%s\n", f);
-    }
-    return (0);
-
-}
-Footer
-© 2022 GitHub, Inc.
